2cb267639dd503bc3fd05b3f66214cce2e468010
dgotlieb <Dgotlieb8@gmail.com>
2020-07-16 20:53:04 +0300

    Update ActionsTest.java

--- ActionsTest.java
+++ ActionsTest.java
@@ -1,8 +1,6 @@
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -12,7 +10,6 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ActionsTest {
     private static ChromeDriver driver;
 
